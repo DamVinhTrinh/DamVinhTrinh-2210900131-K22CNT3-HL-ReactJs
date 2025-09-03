2a1a9c1fcf7c30f84bb0aff6afe8ff01ed2b96fb
DamVinhTrinh <Trinh1569004@gmail.com>
2025-09-03 09:49:07 +0700

    Lesson01

---
+++ dvt-lesson01/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1>Ứng dụng React đầu tiên</h1>
+      <p>Chúc mừng — bạn đã chạy app với J4D$ Shop!</p>
+    </div>
+  )
+}

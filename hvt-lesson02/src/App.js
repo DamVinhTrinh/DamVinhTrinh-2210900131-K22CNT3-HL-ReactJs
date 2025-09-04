b1122e16df2f8bb978dff79dd1ac37af097d4979
DamVinhTrinh <Trinh1569004@gmail.com>
2025-09-04 10:00:51 +0700

    First commit - Demo Function & Class Component

---
+++ hvt-lesson02/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import DvtHelloFunction from "./DvtHelloFunction";
+import DvtHelloClass from "./DvtHelloClass";
+
+function App() {
+  return (
+    <div>
+      <h1>Đây chỉ là demo thôi</h1>
+      <DvtHelloFunction name="Nguyễn Văn A" />
+      <DvtHelloClass name="Trần Thị B" />
+    </div>
+  );
+}
+
+export default App;
